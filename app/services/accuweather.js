42a6cb3b143973b3e0ae23739316a40b515dd88b
Merla <I334691@global.corp.sap>
2016-11-15 16:32:40 +0200

    Initial

--- app/services/accuweather.js
+++ app/services/accuweather.js
@@ -4,19 +4,19 @@ var method = ACWService.prototype;
 function ACWService(_rp) {
     // nothing to do here
     this.rp = _rp;
-    this.ACCUWEATHER_API_KEY = "hoArfRosT1215";
+    this.ACCUWEATHER_API_KEY = "vDabIeXm9GRdW0d0VQ0aYXFacUUJH7KL";
     this.ACCUWEATHER_LANGUAGE = "ro";
 }
 
 method.CityLookUp = function (_q) {
-    var _uri = "http://apidev.accuweather.com/locations/v1/search?q=" +
+    var _uri = "http://dataservice.accuweather.com/locations/v1/search?q=" +
         _q + "&apikey=" + this.ACCUWEATHER_API_KEY;
     console.log("ACW Request CALL => ", _uri);
     return this.rp({ uri: _uri, json: true });
 }
 
 method.GetCurrentConditions = function (_locationKey) {
-    _uri = "http://apidev.accuweather.com/currentconditions/v1/" +
+    _uri = "http://dataservice.accuweather.com/currentconditions/v1/" +
         _locationKey + ".json?language=" + this.ACCUWEATHER_LANGUAGE + "&apikey=" + this.ACCUWEATHER_API_KEY;
     console.log("ACW Request CALL => ", _uri);
     return this.rp({ uri: _uri, json: true });
