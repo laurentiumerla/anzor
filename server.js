18169b693e24b7ff610532d8b10deab0814e5bee
Merla <I334691@global.corp.sap>
2016-11-15 10:51:56 +0200

    Initial

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ router.get('/', function (req, res) {
 // more routes for our API will happen here
 router.route('/acw_location')
     .get(function (req, res) {
+        console.log("Request: ", req)
         var options = {
             uri: 'https://api.projectoxford.ai/luis/v2.0/apps/' +
             LUIS_APP_ID + '?subscription-key=' +
