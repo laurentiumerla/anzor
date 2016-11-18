462f0c37ec781aab92107e160a154787ce3cc7cb
Merla <I334691@global.corp.sap>
2016-11-18 12:15:44 +0200

    Initial

--- app/services/messages.js
+++ app/services/messages.js
@@ -36,11 +36,15 @@ method.ForecastHoursMessage = function (_data, _senderID) {
     var list = {
         "attachment": {
             "type": "template",
-            "payload": {
-                "template_type": "list",
-                "top_element_style": "compact",
-                "elements": []
-            }
+            "payload": []
+        }
+    }
+
+    var payload = {
+        "payload": {
+            "template_type": "list",
+            "top_element_style": "compact",
+            "elements": []
         }
     }
 
@@ -53,16 +57,13 @@ method.ForecastHoursMessage = function (_data, _senderID) {
         if (counter > 4) {
             counter = 1;
             // break;
-            listArray.push(list);
+            list.attachment.payload.push(payload);
 
-            list = {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "list",
-                        "top_element_style": "compact",
-                        "elements": []
-                    }
+            payload = {
+                "payload": {
+                    "template_type": "list",
+                    "top_element_style": "compact",
+                    "elements": []
                 }
             }
         }
@@ -83,13 +84,78 @@ method.ForecastHoursMessage = function (_data, _senderID) {
 
         element.subtitle = item.IconPhrase;
 
-        list.attachment.payload.elements.push(element);
+        payload.elements.push(element);
     }
 
-    listArray.push(list);
+    list.attachment.payload.push(payload);
 
-    // return list;
-    return listArray
+    return list;
+    // return listArray
 }
+
+// method.ForecastHoursMessage = function (_data, _senderID) {
+
+//     var listArray = [];
+//     // var addListToArray = false;
+//     var counter = 0;
+
+//     var list = {
+//         "attachment": {
+//             "type": "template",
+//             "payload": {
+//                 "template_type": "list",
+//                 "top_element_style": "compact",
+//                 "elements": []
+//             }
+//         }
+//     }
+
+//     for (var i = 0, len = _data.length; i < len; i++) {
+//         var item = _data[i];
+//         var d = new Date(item.DateTime);
+//         var h = d.getHours();
+//         counter++;
+
+//         if (counter > 4) {
+//             counter = 1;
+//             // break;
+//             listArray.push(list);
+
+//             list = {
+//                 "attachment": {
+//                     "type": "template",
+//                     "payload": {
+//                         "template_type": "list",
+//                         "top_element_style": "compact",
+//                         "elements": []
+//                     }
+//                 }
+//             }
+//         }
+
+//         var element =
+//             {
+//                 "title": "Classic White T-Shirt",
+//                 "image_url": "https://peterssendreceiveapp.ngrok.io/img/white-t-shirt.png",
+//                 "subtitle": "100% Cotton, 200% Comfortable"
+//             };
+
+//         element.title = "La ora " + h.toString() + ":00 vor fi " + item.Temperature.Value + item.Temperature.Unit;
+//         if (item.WeatherIcon < 10) {
+//             element.image_url = "http://developer.accuweather.com/sites/default/files/0" + item.WeatherIcon + "-s.png";
+//         } else {
+//             element.image_url = "http://developer.accuweather.com/sites/default/files/" + item.WeatherIcon + "-s.png";
+//         }
+
+//         element.subtitle = item.IconPhrase;
+
+//         list.attachment.payload.elements.push(element);
+//     }
+
+//     listArray.push(list);
+
+//     // return list;
+//     return listArray
+// }
 
 module.exports = BotMessage;
