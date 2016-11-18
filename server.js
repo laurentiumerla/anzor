490f448fd6dddd32ab170c3ec35e266198c54237
Merla <I334691@global.corp.sap>
2016-11-18 11:18:54 +0200

    Initial

--- server.js
+++ server.js
@@ -265,7 +265,7 @@ function receivedMessage(event) {
             // if (locationLUIS.length < 1) { locationLUIS.push(req.query.location); }
 
             // console.log(subjectLUIS);
-            luis.setData(data);
+            luis.SetData(data);
             subjectLUIS = luis.GetEntities("Subject");
             locationLUIS = luis.GetEntities("Location");
 
