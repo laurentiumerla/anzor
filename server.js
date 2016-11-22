c734420dddaa927dc016f9723e908b202e66c27d
Merla <I334691@global.corp.sap>
2016-11-22 15:46:51 +0200

    Initial

--- server.js
+++ server.js
@@ -344,7 +344,7 @@ function ACWForecast12Hours(_senderID, _location, _fromCounter) {
                         console.log('qqqqqq', _location)
                         places.textSearch({ query: _location }).then((res) => {
                             var location = res.body.results[0]
-                            console.log('eeeeee', location.photos)
+                            console.log('eeeeee', location.photos[0].photo_reference)
                             sendGenericMessage(_senderID, botmsg.ForecastHoursMessage(data, _senderID, location, _fromCounter));
                         })
                     })
