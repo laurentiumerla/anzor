edd3a4083914d3814f8b1af93a88316ccc621c06
Merla <I334691@global.corp.sap>
2016-11-22 15:52:26 +0200

    Initial

--- app/services/messages.js
+++ app/services/messages.js
@@ -137,7 +137,8 @@ method.ForecastHoursMessage = function (_data, _senderID, _location, _fromCounte
 
     list.attachment.payload.elements.push(
         {
-            "title": "Prognoza pe ore in " + _location.name,
+            // "title": "Prognoza pe ore in " + _location.name,
+            "title": _location.name.toUpperCase(),
             "image_url": mainItem_image_url,
             "subtitle": _data.IconPhrase,
         }
