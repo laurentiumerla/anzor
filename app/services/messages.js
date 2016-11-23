7fa5ff838d2ac9233a51bd8cf3ac1d4c45085633
Merla <I334691@global.corp.sap>
2016-11-23 14:53:33 +0200

    Initial

--- app/services/messages.js
+++ app/services/messages.js
@@ -16,13 +16,16 @@ method.MainMenuNotificationsMessage = function (_notifications, _activeNotificat
         })
     } else message.text = 'Toate notificările sunt dezactivate. \n\n Pe care vrei sa le activezi?'
 
-    console.log(_notifications)
-    if (_notifications) {
-        _notifications.weatherForecasts.options.forEach(function (option) {
 
-            if (option.active == true) {
-                console.log(option)
-            }
+    if (_notifications) {
+        _notifications.forEach(function (notification) {
+            console.log(notification)
+            notification.options.forEach(function (option) {
+                if (option.active == true) {
+                    console.log(option)
+                }
+            })
+
         })
         message.text = '\n\n adsad'
     }
